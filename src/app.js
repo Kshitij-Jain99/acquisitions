61bb396dfed2acd918bfb7bfe589e03b5e2b67bf
Kshitij Jain <kshitijjain6074@gmail.com>
2026-01-11 17:34:07 +0530

    added Authentication

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import logger from '#config/logger.js';
+// import logger from '#config/logger.js';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-    
+// import authRoutes from '#routes/auth.routes.js';
+import logger from './config/logger.js';
+import authRoutes from './routes/auth.routes.js';
+
 const app = express();
 
 app.use(helmet());
@@ -20,5 +23,15 @@ app.get('/', (req, res) => {
       
   res.status(200).send('Hello from Acquisitions!');
 });
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+
+app.get('/api', (req, res) =>{
+    res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
